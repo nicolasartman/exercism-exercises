554371be8b1c399c7c8fec4d308612630e349fea
Nick Artman <nicolasartman@gmail.com>
2017-08-12 00:08:44 -0700

    Complete raindrops exercise

--- elm/raindrops/Raindrops.elm
+++ elm/raindrops/Raindrops.elm
@@ -1,1 +1,39 @@
 module Raindrops exposing (..)
+
+
+factorsAndSounds : List ( number, String )
+factorsAndSounds =
+    [ ( 3, "Pling" )
+    , ( 5, "Plang" )
+    , ( 7, "Plong" )
+    ]
+
+
+produceSound : Int -> ( Int, String ) -> String
+produceSound numberOfDrops ( factor, sound ) =
+    if rem numberOfDrops factor == 0 then
+        sound
+    else
+        ""
+
+
+isNotSilent : String -> Bool
+isNotSilent =
+    not << String.isEmpty
+
+
+raindrops : Int -> String
+raindrops numberOfDrops =
+    let
+        sound =
+            List.map (produceSound numberOfDrops) factorsAndSounds
+                |> List.foldr (++) ""
+    in
+        if sound |> isNotSilent then
+            sound
+        else
+            toString numberOfDrops
+
+
+
+-- http://exercism.io/exercises/elm/raindrops/readme
