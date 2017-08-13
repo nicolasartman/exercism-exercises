28bd47e6ba312fd23d3b8845b52a67d744545edb
Nick Artman <nicolasartman@gmail.com>
2017-08-12 17:36:53 -0700

    Complete scrabble-score exercise in elm

---
+++ elm/scrabble-score/ScrabbleScore.elm
@@ -0,0 +1,55 @@
+module ScrabbleScore exposing (..)
+
+import Dict
+
+
+scores : Dict.Dict Char Int
+scores =
+    Dict.fromList
+        [ ( 'a', 1 )
+        , ( 'e', 1 )
+        , ( 'i', 1 )
+        , ( 'o', 1 )
+        , ( 'u', 1 )
+        , ( 'l', 1 )
+        , ( 'n', 1 )
+        , ( 'r', 1 )
+        , ( 's', 1 )
+        , ( 't', 1 )
+        , ( 'd', 2 )
+        , ( 'g', 2 )
+        , ( 'b', 3 )
+        , ( 'c', 3 )
+        , ( 'm', 3 )
+        , ( 'p', 3 )
+        , ( 'f', 4 )
+        , ( 'h', 4 )
+        , ( 'v', 4 )
+        , ( 'w', 4 )
+        , ( 'y', 4 )
+        , ( 'k', 5 )
+        , ( 'j', 8 )
+        , ( 'x', 8 )
+        , ( 'q', 10 )
+        , ( 'z', 10 )
+        ]
+
+
+scoreLetter : Char -> Int
+scoreLetter letter =
+    Dict.get letter scores |> Maybe.withDefault 0
+
+
+scoreWord : String -> Int
+scoreWord word =
+    let
+        characters =
+            String.toList <| String.toLower <| word
+    in
+        List.foldl
+            (\letter total ->
+                total
+                    + ((\_ -> scoreLetter letter) <| (Debug.log <| toString letter) <| toString <| scoreLetter letter)
+            )
+            0
+            characters
